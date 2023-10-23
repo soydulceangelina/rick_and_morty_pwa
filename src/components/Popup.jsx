0a2726db914b9f305291daed5ad9b8da97096823
soydulceangelina <soydulceangelina@gmail.com>
2023-10-22 20:53:31 -0500

    popUp fix

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -5,25 +5,20 @@ import Card from "./Card";
 import Close from "../assets/close.svg";
 
 export default function Popup({ character, popUp, togglePopUp }) {
-  if (popUp) {
-    document.body.classList.add("active-popUp");
-  } else {
-    document.body.classList.remove("active-popUp");
-  }
-
   return (
     <>
       {popUp && (
         <div className="popUp">
-          <div onClick={togglePopUp} className="overlay"></div>
-          <div className="popUp-content">
-            <Card
-              item={character}
-              allCharacteristic={true}
-              onClick={togglePopUp}
-            />
-            <div className="close-popUp" onClick={togglePopUp}>
-              <img src={Close} alt="close" />
+          <div onClick={togglePopUp} className="overlay">
+            <div className="popUp-content">
+              <Card
+                item={character}
+                allCharacteristic={true}
+                onClick={togglePopUp}
+              />
+              <div className="close-popUp" onClick={togglePopUp}>
+                <img src={Close} alt="close" />
+              </div>
             </div>
           </div>
         </div>
